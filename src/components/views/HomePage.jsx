384bda1152096dbc832e0af8735f1c1fda17b7d3
Carlos Martinez <itscarlosisaac@gmail.com>
2018-08-24 17:37:02 -0400

    Updated homepage.jsx.

--- src/components/views/HomePage.jsx
+++ src/components/views/HomePage.jsx
@@ -4,12 +4,16 @@ export default () => (
   <Fragment>
     <section className="app__container">
       <header>
-        <img width={200} src="https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png" alt="React JS"/>
+        <img
+          width={200}
+          src="https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png"
+          alt="React JS"
+        />
         <h1>Welcome to React</h1>
       </header>
       <p>
         To get started, edit the code from the
-        <code> src/components </code>
+        <code> src/components/views </code>
         and save.
       </p>
     </section>
